12ac6da3ee59d57425f5959c29bbd81ddf11c996
Samat Bekkulov <66533036+SaulBadmannn@users.noreply.github.com>
2021-10-28 03:02:48 +0500

    правки

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private float _health;
+    [SerializeField] private ChangerSliderValue _changerSliderValue;
 
-    public float GetHealth()
+    private float _changeHealthStep = 10f;
+    private float _maxHealth = 100f;
+    private float _minHealth = 0f;
+
+    private void Start()
     {
-        return _health;
+        _changerSliderValue.SetValue(_health);
     }
 
-    public void SetHealth(float health)
+    public void TryGetHeal()
     {
-        _health = health;
+        if(_health < _maxHealth)
+        {
+            _health += _changeHealthStep;
+            _changerSliderValue.IncreaseValue(_health);
+        } 
+    }
+
+    public void TryGetDamage()
+    {
+        if(_health > _minHealth)
+        {
+            _health -= _changeHealthStep;
+            _changerSliderValue.DecreaseValue(_health);
+        }
     }
 }
